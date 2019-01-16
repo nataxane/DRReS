26307efdc558d8100383fde14ee0bc244a430096
OSTAPUK Natalia <ostapukn@diufmac48.unifr.ch>
2019-01-16 15:51:20 +0100

    add locking (go map doesn't support concurrent read/write)

--- core/storage.go
+++ core/storage.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"sync"
 )
 
 type Record string
@@ -14,14 +15,18 @@ type Table map[string]Record
 type Storage struct {
 	tables map[string](Table)
 	logger DBLogger
+	locker *sync.Mutex
 }
 
 func InitStorage() (storage Storage) {
 	dbLogger := initLogger("DRReS.log")
+	locker := &sync.Mutex{}
 
 	storage = Storage{
 		map[string]Table{},
-		dbLogger}
+		dbLogger,
+		locker,
+	}
 
 	log.Print("Recovery started")
 	storage.Recover()
@@ -36,7 +41,9 @@ func (s Storage) Read(tableName string, key string) (value Record, err error) {
 		return "", fmt.Errorf("table %v does not exist", tableName)
 	}
 
+	s.locker.Lock()
 	value, recordOk := table[key]
+	s.locker.Unlock()
 	if recordOk == false {
 		return "", fmt.Errorf("key %v is not in the table", key)
 	} else {
@@ -52,13 +59,17 @@ func (s Storage) Insert(tableName string, key string, value Record) (err error) 
 		s.tables[tableName] = table
 	}
 
+	s.locker.Lock()
 	_, recordOk := table[key]
+	s.locker.Unlock()
 	if recordOk == true {
 		return fmt.Errorf("key %v is already in the table", key)
 	} else {
 		logEntry := fmt.Sprintf("insert %s %s\n", key, value)
 		s.logger.writeToDisk(logEntry)
+		s.locker.Lock()
 		table[key] = value
+		s.locker.Unlock()
 		return
 	}
 }
@@ -70,13 +81,17 @@ func (s Storage) Update(tableName string, key string, value Record) (err error) 
 		return fmt.Errorf("table %v does not exist", tableName)
 	}
 
+	s.locker.Lock()
 	_, recordOk := table[key]
+	s.locker.Unlock()
 	if recordOk == false {
 		return fmt.Errorf("key %v is not in the table", key)
 	} else {
 		logEntry := fmt.Sprintf("update %s %s\n", key, value)
 		s.logger.writeToDisk(logEntry)
+		s.locker.Lock()
 		table[key] = value
+		s.locker.Unlock()
 		return
 	}
 }
@@ -88,13 +103,17 @@ func (s Storage) Delete(tableName string, key string) (err error) {
 		return fmt.Errorf("table %v does not exist", tableName)
 	}
 
+	s.locker.Lock()
 	_, recordOk := table[key]
+	s.locker.Unlock()
 	if recordOk == false {
 		return fmt.Errorf("key %v is not in the table", key)
 	} else {
 		logEntry := fmt.Sprintf("delete %s\n", key)
 		s.logger.writeToDisk(logEntry)
+		s.locker.Lock()
 		delete(table, key)
+		s.locker.Unlock()
 		return nil
 	}
 }
