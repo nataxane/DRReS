2d4c02f0d8f0a3fafbd2837d7f629d7c302ce234
OSTAPUK Natalia <ostapukn@diufmac48.unifr.ch>
2019-01-16 16:38:18 +0100

    minor fixes: add consts, move recovery to a seperate file

---
+++ core/recovery.go
@@ -0,0 +1,41 @@
+package core
+
+import (
+	"bufio"
+	"log"
+	"strings"
+)
+
+func redoLog(s Storage) {
+	logScanner := bufio.NewScanner(s.logger.logFile)
+
+	for logScanner.Scan() {
+		logEntry := logScanner.Text()
+		query := &strings.SplitN(logEntry, " ", 3)[2]
+
+		op, key, value := parseQuery(*query)
+		tableName := "default"
+
+		table, tableOk := s.tables[tableName]
+
+		if tableOk == false {
+			table = Table{}
+			s.tables[tableName] = table
+		}
+
+		switch {
+		case op == "insert" || op =="update":
+			table[key] = Record(value)
+		case op == "delete":
+			delete(table, key)
+		default:
+			log.Printf("Skip invalid query in the log: %v", query)
+		}
+	}
+}
+
+func (s Storage) Recover() {
+	redoLog(s)
+
+	log.Printf("Successfully recovered %d rows", len(s.tables["default"]))
+}
